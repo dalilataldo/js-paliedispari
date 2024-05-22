d713b835ded517b8caf607a2a14fe51a5ec1c74d
dalilataldo <dalila.taldo@gmail.com>
2024-05-22 11:43:06 +0200

    add: modifications

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,31 +1,26 @@
-function palindromeword(word) {
-    // chiedo una parola all'utente
-    // let word = prompt('Inserisci una parola');
 
-    // divido la stringa in lettere creando un array
-    let dividedword = word.toLowerCase().split('');
-    console.log(dividedword);
+// funzione: 
+// divido la stringa in lettere creando un array
+// applico la proprietà reverse per invertire l'ordine degli elementi dell'array
+// trasformo in stringa l'array invertito
+// restituisco se le due parole sono uguali
 
-    // applico la proprietà reverse per invertire l'ordine degli elementi dell'array
-    dividedword = dividedword.reverse();
-    console.log(dividedword)
+function isPalindrome(word) {
+    const reversedWord = word.toLowerCase().split('').reverse().join('');
 
-    // trasformo in stringa l'array invertito
-    let reverseword = dividedword.join('');
-    console.log(reverseword);
-
-    // comparazione tra la parola e la stessa parola in senso invertito --> stampo risultato uguaglianza
-    if (isNaN(word)) {
-        if (word === reverseword) {
-            return 'La parola è palindroma';
-        } else {
-            return 'La parola non è palindroma';
-        }
-    }
-    else {
-        return 'Non hai inserito una parola'
-    }
+    return reversedWord === word.toLowerCase();
 }
+//input: chiedo una parola all'utente
+const userWord = prompt('Inserisci una parola');
 
-const result = palindromeword('ciao')
-console.log(result);
+//svolgimento
+if (!isNaN(userWord)) {
+    alert('La parola non è valida')
+} else {
+
+    let result;
+    if (isPalindrome(userWord)) result = 'La parola è palindroma';
+    else result = 'La parola non è palindroma';
+
+    console.log(result);
+}
